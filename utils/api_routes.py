a0136330ac5f6b513bb678801370e94f313459f2
nadingomaa <155377451+nadingomaapianat@users.noreply.github.com>
2026-03-27 06:25:52 +0200

    comply

--- utils/api_routes.py
+++ utils/api_routes.py
@@ -6,6 +6,7 @@ from routes.control_api_routes import router as control_router
 from routes.risk_api_routes import router as risk_router
 from routes.incident_api_routes import router as incident_router
 from routes.kri_api_routes import router as kri_router
+from routes.comply_api_routes import router as comply_router
 from routes.report_api_routes import router as report_router
 from routes.bank_check_api_routes import router as bank_check_router
 from routes.dynamic_report_endpoints import router as dynamic_router
@@ -23,6 +24,7 @@ router.include_router(control_router)
 router.include_router(risk_router)
 router.include_router(incident_router)
 router.include_router(kri_router)
+router.include_router(comply_router)
 router.include_router(report_router)
 router.include_router(bank_check_router)
 router.include_router(dynamic_router)
